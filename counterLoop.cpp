799a87a530489ac0f7f45525f225be33f6adb18b
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-03 10:26:17 -0800

    Add files via upload

---
+++ counterLoop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+		int number=0;
+		while(number !=-1)
+		{
+			cout<<"Please Enter any Number or -1 to Exit: ";
+			cin>>number;
+		}
+			cout<<"Program Ends";
+}
